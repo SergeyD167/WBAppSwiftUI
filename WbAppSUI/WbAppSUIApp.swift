6f629f487f76f249a1c166a3af3abfb44e852014
Sergey Dyatlov <sergeyd020501@gmail.com>
2024-06-16 16:58:35 +0300

    Initial Commit

---
+++ WbAppSUI/WbAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WbAppSUIApp.swift
+//  WbAppSUI
+//
+//  Created by Сергей Дятлов on 16.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WbAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
